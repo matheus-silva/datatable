4b51f6600bab272518dd41c86f2c6ba0620825ab
Matheus da Silva Ferreira <matheus.dasilva.ferreira@hotmail.com>
2015-09-29 21:20:38 -0300

    Questões 1, 2 e 3 resolvidas

--- src/com/edupsousa/datatable/DataTable.java
+++ src/com/edupsousa/datatable/DataTable.java
@@ -2,18 +2,19 @@ package com.edupsousa.datatable;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.Objects;
 
 public class DataTable {
 
 	public static final int TYPE_INT = 0;
 	public static final int TYPE_STRING = 1;
-	
+
 	public static final int FORMAT_CSV = 0;
 	public static final int FORMAT_HTML = 1;
-	
+
 	private LinkedHashMap<String, Integer> columnsTypes = new LinkedHashMap<String, Integer>();
 	private ArrayList<DataTableRow> rows = new ArrayList<DataTableRow>();
-	
+
 	public int columnsCount() {
 		return columnsTypes.size();
 	}
@@ -40,22 +41,24 @@ public class DataTable {
 	}
 
 	public DataTableRow lastRow() {
-		return rows.get(rows.size()-1);
+		return rows.get(rows.size() - 1);
 	}
 
 	public int getCollumnType(String collumn) {
 		return columnsTypes.get(collumn);
 	}
-	
+
 	private void checkRowCompatibilityAndThrows(DataTableRow row) {
 		for (String collumnName : columnsTypes.keySet()) {
-			if (row.hasValueFor(collumnName) && 
-					!(isValueCompatible(columnsTypes.get(collumnName), row.getValue(collumnName)))) {
-				throw new ClassCastException("Wrong type for collumn " + collumnName + ".");
+			if (row.hasValueFor(collumnName)
+					&& !(isValueCompatible(columnsTypes.get(collumnName),
+							row.getValue(collumnName)))) {
+				throw new ClassCastException("Wrong type for collumn "
+						+ collumnName + ".");
 			}
 		}
 	}
-	
+
 	private boolean isValueCompatible(int type, Object value) {
 		if (type == this.TYPE_INT && !(value instanceof Integer)) {
 			return false;
@@ -88,19 +91,82 @@ public class DataTable {
 				}
 				output += "\n";
 			}
+		} else if (format == DataTable.FORMAT_HTML) {
+			output += "<table>\n<tr>";
+			for (String collumName : columnsTypes.keySet()) {
+				output += "<td>" + collumName + "</td>";
+			}
+			output += "</tr>\n";
+			for (int i = 0; i < this.rowsCount(); i++) {
+				output += "<tr>";
+				row = this.getRow(i);
+				for (String collumName : columnsTypes.keySet()) {
+					output += "<td>" + row.getValue(collumName) + "</td>";
+				}
+				output += "</tr>\n";
+			}
+			output += "</table>\n";
 		}
 		return output;
 	}
-	
+
 	public void insertRowAt(DataTableRow row, int index) {
 		rows.add(index, row);
 	}
-	
+
 	public DataTable filterEqual(String collumn, Object value) {
-		return null;
+		DataTable output = new DataTable();
+
+		for (String collumName : columnsTypes.keySet()) {
+			output.addCollumn(collumName, columnsTypes.get(collumName));
+		}
+
+		for (int i = 0; i < this.rowsCount(); i++) {
+			DataTableRow rowTemp = this.getRow(i);
+
+			if (Objects.equals(rowTemp.getValue(collumn), value)) {
+				DataTableRow row = output.createRow();
+
+				for (String collumName : columnsTypes.keySet()) {
+					row.setValue(collumName, rowTemp.getValue(collumName));
+				}
+
+				output.insertRow(row);
+			}
+		}
+		return output;
 	}
-	
+
 	public DataTable sortAscending(String collumn) {
-		return null;
+		if (columnsTypes.get(collumn) == TYPE_STRING) {
+			throw new ClassCastException("Only Integer columns can be sorted.");
+		}
+
+		DataTable output = new DataTable();
+
+		for (String collumName : columnsTypes.keySet()) {
+			output.addCollumn(collumName, columnsTypes.get(collumName));
+		}
+
+		for(int i = 0; i < this.rowsCount(); i++){
+			DataTableRow rowTemp= this.getRow(i);
+			DataTableRow row;
+			int j = 0;
+			for(j = 0; j < output.rowsCount(); j++){
+				row = output.getRow(j);
+				if((Integer) (rowTemp.getValue(collumn)) < (Integer) row.getValue(collumn)){
+					break;
+				}
+			}
+			row = output.createRow();
+			
+			for (String collumName : columnsTypes.keySet()) {
+				row.setValue(collumName, rowTemp.getValue(collumName));
+			}
+			
+			output.insertRowAt(row, j);
+		}
+		
+		return output;
 	}
 }
